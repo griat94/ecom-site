cfc8cc6d4bf45b7a3385e65325a374ebb5855381
griat94 <gunreet.riat@gmail.com>
2023-10-20 00:07:20 -0500

    minor style changes

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,24 +5,30 @@ import { useNavigate } from 'react-router-dom'
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
 
-import { CartDropdownContainer, CartItems } from './cart-dropdown.styles'
+import {
+  CartDropdownContainer,
+  CartItems,
+  EmptyMessage,
+} from './cart-dropdown.styles'
 
 const CartDropdown = () => {
   const navigate = useNavigate()
   const { cartItems } = useContext(CartContext)
 
-  const navigateToCheckout = () => {
+  const navigateToCheckoutHandler = () => {
     navigate('/checkout')
   }
 
   return (
     <CartDropdownContainer>
       <CartItems>
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
       </CartItems>
-      <Button onClick={navigateToCheckout}>CHECKOUT</Button>
+      <Button onClick={navigateToCheckoutHandler}>CHECKOUT</Button>
     </CartDropdownContainer>
   )
 }
